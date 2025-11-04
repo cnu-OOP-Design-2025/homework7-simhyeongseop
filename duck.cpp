0b4f10472eefb0407bfcdb76bc4adde4a1678602
simhyeongseop <202002513@o.cnu.ac.kr>
2025-11-04 13:38:30 +0900

    과제 제출

--- duck.cpp
+++ duck.cpp
@@ -1,22 +1,39 @@
 #include "duck.h"
 
-/* TODO */
-void Duck::performFly(){}
-/* TODO */
-void Duck::performQuack(){}
-/* TODO */
-void Duck::setFlyBehavior(FlyBehavior* fb){}
-/* TODO */
-void Duck::setQuackBehavior(QuackBehavior* qb){}
+void Duck::performFly(){
+    if(flyBehavior)
+        flyBehavior->fly();
+}
+
+void Duck::performQuack(){
+    if(quackBehavior)
+        quackBehavior->quack();
+}
+
+void Duck::setFlyBehavior(FlyBehavior* fb){
+    if(flyBehavior)
+        delete flyBehavior;
+    flyBehavior = fb;
+}
+
+void Duck::setQuackBehavior(QuackBehavior* qb){
+    if(quackBehavior) 
+        delete quackBehavior;
+    quackBehavior = qb;
+}
 
 
-/* TODO */
-MallardDuck::MallardDuck() {}
-/* TODO */
-RedheadDuck::RedheadDuck() {}
-/* TODO */
-RubberDuck::RubberDuck() {}
-/* TODO */
-DecoyDuck::DecoyDuck() {}
-/* TODO */
-ModelDuck::ModelDuck() {}
+MallardDuck::MallardDuck()
+    : Duck(new FlyWithWings(), new Quack()) {}
+
+RedheadDuck::RedheadDuck()
+    : Duck(new FlyWithWings(), new Quack()) {}
+
+RubberDuck::RubberDuck()
+    : Duck(new FlyNoWay(), new Squeak()) {}
+
+DecoyDuck::DecoyDuck()
+    : Duck(new FlyNoWay(), new MuteQuack()) {}
+
+ModelDuck::ModelDuck()
+    : Duck(new FlyNoWay(), new MuteQuack()) {}
